2865667a59e9cb1db3b285291590784c035460b7
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-01-04 02:43:36 +0900

    feat: implement Success component

---
+++ src/popup/Success.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Icon } from "react-lightning-design-system";
+
+export const Success = () => {
+  return (
+    <Icon category="action" icon="approval" container="circle" size="large" />
+  );
+};
