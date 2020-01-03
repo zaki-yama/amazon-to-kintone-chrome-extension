d35ea9cf9450bdd712ecd07eb260066654718855
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-01-04 02:43:13 +0900

    feat: update storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,3 @@
 import { configure } from "@storybook/react";
 
-const req = require.context("../src", true, /\.stories\.tsx$/);
-function loadStories() {
-  req.keys().forEach(req);
-}
-configure(loadStories, module);
+configure(require.context("../src", true, /\.stories\.tsx$/), module);
